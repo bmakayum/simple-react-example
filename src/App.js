02196bcb77daccf1383982b5063e9605aa388e34
bmakayum <54470910+bmakayum@users.noreply.github.com>
2019-08-25 08:31:21 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Akm from './components/akm/Akm.js';
+import Nav from './components/nav/Nav.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Akm />
+    </div>
+  );
+}
+
+
+export default App;
